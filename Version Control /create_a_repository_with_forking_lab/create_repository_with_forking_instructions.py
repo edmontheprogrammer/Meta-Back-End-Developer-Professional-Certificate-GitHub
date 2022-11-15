263a37394807f37ac46614f37264b8d6d4de0e5c
Edmon the Programmer <edmontheprogrammer@gmail.com>
2022-11-15 07:31:16 +0200

    Forking and cloning repos commit
    Forking and cloning repos commit

---
+++ Version Control /create_a_repository_with_forking_lab/create_repository_with_forking_instructions.py
@@ -0,0 +1,21 @@
+# Create a Repository with Forking: 
+# 
+# Instructions:
+#   
+#   In this ungraded lab you will practice version control by forking a repository, creating a branch and commiting a 
+# change. Once your changes are staged you will open a pull request with the source repo. 
+# 
+# Goal:
+#   
+#   * Create a pull request from a forked repository. 
+# 
+# Objectives:
+#   
+#   * Fork a repo 
+#   * Clone a forked repo locally 
+#   * Create a branch 
+#   * Add and commit changes 
+#   * Push changes 
+#   * Open a pull request 
+# 
+# ## 
