bb48d2666398b87e03d0f43912946f228a1d28a2
Edmon the Programmer <41594880+edmontheprogrammer@users.noreply.github.com>
2022-12-21 07:55:34 +0200

    api dev. updates

--- APIs/book-list-api/book_list_api_project/book_list_api_project/settings.py
+++ APIs/book-list-api/book_list_api_project/book_list_api_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # custom installed apps
+    "book_list_api_app",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+
+
 ]
 
 ROOT_URLCONF = "book_list_api_project.urls"
